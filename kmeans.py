cfff06ba2e8f2a663cda8af34bf2cdf5aa0dd245
Kyungwon Chun <ruddyscent@gmail.com>
2020-03-15 19:43:19 +0900

    Remove debugging outputs

--- kmeans.py
+++ kmeans.py
@@ -15,7 +15,6 @@ class Model(tf.keras.Model):
         super(Model, self).__init__()
         vector_a = tf.random.normal((NUM_POINTS // 2, 2), mean=0, stddev=0.9)
         vector_b = tf.random.normal((NUM_POINTS // 2, 2), mean=[3, 1], stddev=0.5)
-        print(vector_b.dtype)
         self.vectors = tf.random.shuffle(tf.concat([vector_a, vector_b], 0))
         self.centroids = self.vectors[:K, :]
         self.assignment = tf.random.uniform([NUM_POINTS], minval=0, maxval=K, dtype=tf.int32)
@@ -27,7 +26,6 @@ class Model(tf.keras.Model):
             pts = tf.gather(self.vectors, pts_idx)
             centroids.append(tf.reduce_mean(pts, 1))
         self.centroids = tf.reshape(tf.convert_to_tensor(centroids, dtype=tf.float32), [K, 2])
-        print(self.centroids)
         expanded_vectors = tf.expand_dims(self.vectors, 0)
         expanded_centroides = tf.expand_dims(self.centroids, 1)
         distances = tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroides)), 2)
@@ -37,7 +35,6 @@ model = Model()
 for i in range(1):
     model.update_centroids()
 
-print(model.vectors[:, 0].shape, model.vectors[:, 1].shape, model.assignment.numpy().shape)
 df = pd.DataFrame({"x": model.vectors[:, 0], "y": model.vectors[:, 1], "cluster": model.assignment.numpy()})
 sns.lmplot("x", "y", data=df, fit_reg=False, size=6, hue="cluster")
 plt.show()
